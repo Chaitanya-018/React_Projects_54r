d1973c279d9250f034c6d8bbb9ba881b1e801672
soma chaitanya <nammisomachaitanya@gmail.com>
2025-12-20 13:16:41 +0530

    list/object rendering

---
+++ Day6/listrendering/src/App.jsx
@@ -0,0 +1,15 @@
+import Listrendering1 from "./Components/Listrendering1"
+import Listrendering2 from "./Components/Listrendering2"
+
+
+function App() {
+ 
+  return (
+    <>
+      <Listrendering1 />
+      <Listrendering2 />
+    </>
+  )
+}
+
+export default App
